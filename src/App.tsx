ff9c1fb6aae3dc18fa72d4da9e1063ab6a4d8c18
BillStephens2022 <two4onebill@yahoo.com>
2024-02-14 09:39:14 -0500

    added loading state and error handling along with rendering a message if data is loading or if there is an error

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useEffect, useState } from "react";
 import { get } from "./util/http";
 import BlogPosts, { type BlogPost } from "./components/BlogPosts";
 import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPosts = {
   id: number;
@@ -12,9 +13,13 @@ type RawDataBlogPosts = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
+      try {
       const data = (await get(
         "https://jsonplaceholder.typicode.com/posts"
       )) as RawDataBlogPosts[];
@@ -27,14 +32,29 @@ function App() {
         }
       });
       setFetchedPosts(blogPosts);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+      // setError("Failed to fetch posts!");
+    }
+      setIsFetching(false);
     }
     fetchPosts();
   }, []);
 
   let content: ReactNode;
 
+  if (error) {
+    content = <ErrorMessage text={error} />
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>;
   }
 
   return (
